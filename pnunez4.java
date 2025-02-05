89507e4a0c9a9e10b2c2e537de1e1d7452a0dc94
pnunez <pnunez14@toromail.csudh.edu>
2025-02-05 02:29:40 +0000

    menu version 1

---
+++ pnunez4.java
@@ -0,0 +1,44 @@
+//Pedro Nunez
+/*csc-321
+ * lab #4
+*/
+
+import java.util.Scanner;
+
+public class pnunez4
+{
+	public static void main(String[] args)
+	{
+		int x = 0;
+
+		System.out.println("Enter any choice you want from option 1-4 ");
+		System.out.println("Halo ");
+		System.out.println("Destiny 2 ");
+		System.out.println("Divison 2 ");
+		System.out.println("DOOM ");
+
+		Scanner key = new Scanner(System.in);
+		x = key.nextInt();
+
+		System.out.println("You picked option "+x);
+	
+	
+	
+	
+	
+	
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

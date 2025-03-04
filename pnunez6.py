3778a81445e9a33333e5ace080fb49f5650db0d4
pnunez <pnunez14@toromail.csudh.edu>
2025-03-04 23:09:41 +0000

    lab6

---
+++ pnunez6.py
@@ -0,0 +1,27 @@
+"""
+Pedro Nunez
+csc-321 
+lab #6
+"""
+
+
+x = 7
+y = 7.7
+z = "Destiny"
+
+if x > 10:
+    print("Variable x is greater than 10")
+else:
+    print("Variable x is not greater than 10")
+
+
+if y < 5.5:
+    print("Variable y is less than 5.5")
+else:
+    print("Variable y is greater than 5.5")
+
+if z == "Destiny":
+    print("Variable z is Destiny")
+else:
+    print("Variable z is not Destiny")
+
